6b6c9804eb7f4df5cd9fa5a8fb74854e1072918f
Aklev <diego.rodriguez25@uptc.edu.co>
2026-09-01 20:32:24 -0500

    03_Condicionales.py

--- 03_Condicionales.py
+++ 03_Condicionales.py
@@ -2,14 +2,78 @@
 # Diego Alejandro Rodriguez Araque_ 202114910
 # Apuntes parte de digitos variables con limites
 # Online Python compiler (interpreter) to run Python online.
-nota= float(input ("Nota:"))
-if nota < 0 or nota>5:
-    print ("Nota no valida")
-elif nota < 3:
-    print("Insuficiente")
-elif nota <4:
-    print ("Basico")
-elif nota < 4.6:
-    print ("Alto")
-else:
-    print("Superior") 
+#nota= float(input ("Nota:"))
+#if nota < 0 or nota>5:
+#    print ("Nota no valida")
+#elif nota < 3:
+#    print("Insuficiente")
+#elif nota <4:
+#    print ("Basico")
+#elif nota < 4.6:
+#    print ("Alto")
+#else:
+#    print("Superior") 
+# edad =25
+# matricula ="si"
+# contraseña = "azul21"
+# if edad < 18:
+#     print("Acceso restringido")
+# else:
+#         if matricula == "si":
+#             if contraseña == "azul21":
+#                 print("Bienvenido")
+#             else :
+#                  print("Contraseña incorrecta")
+#         else:
+#                 print("No tiene matricula")
+     
+# Nombre = input("Nombre: ")
+# Edad = int(input("Edad: "))
+# tiene_invitacion = input("¿Tiene invitación? (si/no): ")
+# invitacion= tiene_invitacion.lower() 
+# if Edad >= 18 and tiene_invitacion == "si":
+#      print("Autorizado ", Nombre)
+# elif Edad <= 18:
+#     print("acceso denegado, ", Nombre)
+# else:
+#         print("Necesita invitación, ", Nombre)
+
+# numero = 1
+# while numero <= 5:
+#     print(numero)
+#     numero += 1
+
+# numero = 1
+# while numero <= 3:
+#     print(numero) # nunca se detiene el ciclo
+    
+# contraseña = "" 
+# while contraseña != "phyton":
+#     contraseña = input("contraseña: ")
+
+# print("Bienvenido.") # nunca se detiene el ciclo/ while es ideal para un gran numero de intentos
+
+contraseña = "" 
+intentos = 0
+
+# while contraseña != "phyton" and intentos < 3:
+#     contraseña = input("contraseña: ")
+#     intentos += 1
+
+# if contraseña == "phyton":
+#     print(" Acesso Autorizado")
+# else:
+#     print("Acceso bloquedo")
+
+pin_correcto = "2580"
+max_intentos = 3
+intentos = 0
+while intentos < max_intentos:
+    pin_ingresado = input("Ingrese el PIN: ")
+    if pin_ingresado == pin_correcto:
+        print("Acceso autorizado")
+        intentos +=1
+    else:
+        print("PIN incorrecto")
+        intentos += 1
+        
